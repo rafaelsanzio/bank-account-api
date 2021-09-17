a46e7f92f4071dc76769fd6e00a07eea1f180589
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-09-17 09:01:04 -0300

    refactor: moving dtos, routes, enum and logger into new pastes
    refactor: moving dtos, routes, enum and logger into new pastes

--- dtos/IAccountDTO.ts
+++
@@ -1,5 +0,0 @@
-export default interface IAccountDTO {
-  id: number;
-  name: string;
-  balance: number;
-}
--- enum.ts
+++
@@ -1,3 +0,0 @@
-const accountFileJson = "accounts.json";
-
-export { accountFileJson };
