3bfa3ef337951ec5cecf1bff207c130da8ab625a
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-09-25 16:06:26 -0300

    feat: adding new routes for transactions and transfers
    feat: adding new routes for transactions and transfers

--- src/shared/infra/http/routes/accounts.routes.ts
+++ src/shared/infra/http/routes/accounts.routes.ts
@@ -5,6 +5,8 @@ import { ListAccountController } from '@modules/accounts/useCases/listAccounts/L
 import { CreateAccountController } from '@modules/accounts/useCases/createAccount/CreateAccountController';
 import { DeleteAccountController } from '@modules/accounts/useCases/deleteAccount/DeleteAccountController';
 import { UpdateAccountController } from '@modules/accounts/useCases/updateAccount/UpdateAccountController';
+import { TransactionAccountController } from '@modules/accounts/useCases/transactionsInAccount/TransactionAccountController';
+import { TransferAccountsController } from '@modules/accounts/useCases/transferAccounts/TransferAccountsController';
 
 import { AccountValidator } from '../validators/accounts.validator';
 
@@ -17,6 +19,8 @@ const getAccountController = new GetAccountController();
 const listAccountsController = new ListAccountController();
 const deleteAccountController = new DeleteAccountController();
 const updateAccountController = new UpdateAccountController();
+const transactionAccountController = new TransactionAccountController();
+const transferAccountsController = new TransferAccountsController();
 
 accountsRouter.post(
 	'/',
@@ -35,114 +39,15 @@ accountsRouter.put(
 	accountsValidator.put,
 	updateAccountController.handle,
 );
-
-/*
-router.post("/transaction", async (request: Request, response: Response) => {
-  try {
-    let { id, value } = request.body;
-    let data = await fs.readFile(accountFileJson, "utf-8");
-    let result = JSON.parse(data);
-
-    id = Number(id);
-    value = Number(value);
-
-    const index = result.accounts.findIndex(
-      (account: IAccountDTO) => account.id === id
-    );
-    if (index === -1) {
-      logger.error(`POST /accounts/transaction - Not Found`);
-      response.status(400).send({ error: "Not Found" });
-      return;
-    }
-
-    if (value < 0 && result.accounts[index].balance + value < 0) {
-      logger.error(`POST /accounts/transaction - Insufficient Funds`);
-      throw new Error("Insufficient Funds");
-    }
-
-    result.accounts[index].balance += value;
-
-    await fs
-      .writeFile(accountFileJson, JSON.stringify(result))
-      .then(() => {
-        logger.info(
-          `POST /accounts/transaction - ${JSON.stringify(
-            result.accounts[index]
-          )}`
-        );
-        response.send(result.accounts[index]);
-      })
-      .catch((err) => {
-        logger.error(`POST /accounts/transaction - ${err.message}`);
-        response.status(400).send({ error: err.message });
-      });
-  } catch (err) {
-    logger.error(`POST /accounts/transaction - ${err.message}`);
-    response.status(400).send({ error: err.message });
-  }
-});
-
-router.post("/transfer", async (request: Request, response: Response) => {
-  try {
-    let { to, from, value } = request.body;
-    let data = await fs.readFile(accountFileJson, "utf-8");
-    let result = JSON.parse(data);
-
-    const toID = Number(to);
-    const fromID = Number(from);
-    value = Number(value);
-
-    if (value <= 0) {
-      logger.error(
-        `POST /accounts/transfer - Operation is not valid, negative value to transfer`
-      );
-      throw new Error("Operation is not valid, negative value to transfer");
-    }
-
-    const toIndex = result.accounts.findIndex(
-      (account: IAccountDTO) => account.id === toID
-    );
-    if (toIndex === -1) {
-      logger.error(`POST /accounts/transfer - To Account Not Found`);
-      response.status(400).send({ error: "To Account Not Found" });
-      return;
-    }
-
-    const fromIndex = result.accounts.findIndex(
-      (account: IAccountDTO) => account.id === fromID
-    );
-    if (fromIndex === -1) {
-      logger.error(`POST /accounts/transfer - From Account Not Found`);
-      response.status(400).send({ error: "From Account Not Found" });
-      return;
-    }
-
-    if (result.accounts[toIndex].balance - value < 0) {
-      logger.error(`POST /accounts/transfer - Insufficient Funds`);
-      throw new Error("Insufficient Funds");
-    }
-
-    result.accounts[toIndex].balance -= value;
-    result.accounts[fromIndex].balance += value;
-
-    await fs
-      .writeFile(accountFileJson, JSON.stringify(result))
-      .then(() => {
-        logger.info(
-          `POST /accounts/transfer - ${JSON.stringify(
-            result.accounts[toIndex]
-          )}`
-        );
-        response.send(result.accounts[toIndex]);
-      })
-      .catch((err) => {
-        logger.error(`POST /accounts/transfer - ${err.message}`);
-        response.status(400).send({ error: err.message });
-      });
-  } catch (err) {
-    logger.error(`POST /accounts/transfer - ${err.message}`);
-    response.status(400).send({ error: err.message });
-  }
-}); */
+accountsRouter.post(
+	'/transaction',
+	accountsValidator.transaction,
+	transactionAccountController.handle,
+);
+accountsRouter.post(
+	'/transfer',
+	accountsValidator.transfer,
+	transferAccountsController.handle,
+);
 
 export default accountsRouter;
