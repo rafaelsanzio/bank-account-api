90446ddc994bf041161194966928b8119c834472
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-09-25 16:08:22 -0300

    feat: updating interface of accounts repository
    feat: updating interface of accounts repository

--- src/modules/accounts/repositories/IAccountsRepository.ts
+++ src/modules/accounts/repositories/IAccountsRepository.ts
@@ -1,7 +1,10 @@
 import Account from '../infra/filejson/model/Account';
+
 import IAccountDTO from '../dtos/IAccountDTO';
 import IUpdateAccountDTO from '../dtos/IUpdateAccountDTO';
 import IQueryParamsAccountDTO from '../dtos/IQueryParamsAccountDTO';
+import ITransactionAccountDTO from '../dtos/ITransactionAccountDTO';
+import ITransferAccountDTO from '../dtos/ITransferAccountDTO';
 
 export default interface IAccountRepository {
 	create(data: IAccountDTO): Promise<Account>;
@@ -10,4 +13,6 @@ export default interface IAccountRepository {
 	delete(id: string): Promise<Account | undefined>;
 	update(id: string, data: IUpdateAccountDTO): Promise<Account | undefined>;
 	findByNumber(number: number): Promise<Account | undefined>;
+	transaction(data: ITransactionAccountDTO): Promise<Account>;
+	transfer(data: ITransferAccountDTO): Promise<void>;
 }
