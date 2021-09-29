d9bbe6c68d46f67798c246dc22108ed86beb01e1
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-09-29 09:53:10 -0300

    test: adding unit test for creating account
    test: adding unit test for creating account

---
+++ src/modules/accounts/useCases/createAccount/CreateAccountUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { AccountsRepositoryInMemory } from '@modules/accounts/repositories/in-memory/AccountsRepositoryInMemory';
+import AppError from '@shared/errors/AppError';
+
+import { CreateAccountUseCase } from './CreateAccountUseCase';
+
+let createAccountUseCase: CreateAccountUseCase;
+let accountsRepositoryInMemory: AccountsRepositoryInMemory;
+
+describe('Create Car', () => {
+	beforeEach(() => {
+		accountsRepositoryInMemory = new AccountsRepositoryInMemory();
+		createAccountUseCase = new CreateAccountUseCase(accountsRepositoryInMemory);
+	});
+
+	test('should be able to create a new account', async () => {
+		const account = await createAccountUseCase.execute({
+			name: 'Any Account',
+			number: 123456,
+			balance: 90,
+		});
+
+		expect(account).toHaveProperty('id');
+	});
+
+	test('should not able to create a new account with same number', async () => {
+		await createAccountUseCase.execute({
+			name: 'Any Account',
+			number: 123456,
+			balance: 90,
+		});
+
+		await expect(
+			createAccountUseCase.execute({
+				name: 'Any Account 2',
+				number: 123456,
+				balance: 190,
+			}),
+		).rejects.toEqual(new AppError('Account already exist!'));
+	});
+});
