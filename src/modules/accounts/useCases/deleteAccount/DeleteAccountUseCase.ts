c1c2d2a54e9283684b7acf0b76121fc56c3fe862
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-09-30 09:13:59 -0300

    refactor: adding log dependecy (Delete account)
    refactor: adding log dependecy (Delete account)

--- src/modules/accounts/useCases/deleteAccount/DeleteAccountUseCase.ts
+++ src/modules/accounts/useCases/deleteAccount/DeleteAccountUseCase.ts
@@ -1,24 +1,26 @@
+import winston from 'winston';
 import { inject, injectable } from 'tsyringe';
+
 import AppError from '@shared/errors/AppError';
 
 import IAccountRepository from '@modules/accounts/repositories/IAccountsRepository';
-import logger from '@shared/infra/log/logger';
 
 @injectable()
 export class DeleteAccountUseCase {
 	constructor(
 		@inject('IAccountRepository')
 		private accountRepository: IAccountRepository,
+		private log: winston.Logger,
 	) {}
 	async execute(id: string): Promise<void> {
 		const accountExists = await this.accountRepository.get(id);
 
 		if (!accountExists) {
-			logger.error(`DELETE /accounts:${id} - Account ${id} does not found`);
+			this.log.error(`DELETE /accounts:${id} - Account ${id} does not found`);
 			throw new AppError('Account does not exist!');
 		}
 
 		await this.accountRepository.delete(id);
-		logger.info(`DELETE /accounts/${id} - ${JSON.stringify(accountExists)}`);
+		this.log.info(`DELETE /accounts/${id} - ${JSON.stringify(accountExists)}`);
 	}
 }
