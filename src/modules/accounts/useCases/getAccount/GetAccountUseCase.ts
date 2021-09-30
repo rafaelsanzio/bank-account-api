2ff623b5c6770cbe1489f22bcfbb2eccb02cf555
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-09-30 09:14:20 -0300

    refactor: adding log dependency (Get account)
    refactor: adding log dependency (Get account)

--- src/modules/accounts/useCases/getAccount/GetAccountUseCase.ts
+++ src/modules/accounts/useCases/getAccount/GetAccountUseCase.ts
@@ -1,7 +1,7 @@
+import winston from 'winston';
 import { inject, injectable } from 'tsyringe';
-import AppError from '@shared/errors/AppError';
 
-import logger from '@shared/infra/log/logger';
+import AppError from '@shared/errors/AppError';
 
 import Account from '@modules/accounts/infra/filejson/model/Account';
 import IAccountRepository from '@modules/accounts/repositories/IAccountsRepository';
@@ -11,15 +11,16 @@ export class GetAccountUseCase {
 	constructor(
 		@inject('IAccountRepository')
 		private accountRepository: IAccountRepository,
+		private log: winston.Logger,
 	) {}
 	async execute(id: string): Promise<Account | undefined> {
 		const account = await this.accountRepository.get(id);
 		if (!account) {
-			logger.error(`GET /accounts:${id} - Not Found`);
+			this.log.error(`GET /accounts:${id} - Not Found`);
 			throw new AppError('Account does not found!');
 		}
 
-		logger.info(`GET /accounts:id - ${JSON.stringify(account)}`);
+		this.log.info(`GET /accounts:id - ${JSON.stringify(account)}`);
 		return account;
 	}
 }
